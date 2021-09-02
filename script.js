77220530f7c1f0d21dd3fe3f36f584bd80a03e51
Coding2Coding <76713830+Coding2Coding@users.noreply.github.com>
2021-09-01 16:30:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function addUsername() {
+    var username = document.getElementById("usernameInput").value;
+    localStorage.setItem("username", username);
+    window.location = "kwitterRoom.html"
+}
